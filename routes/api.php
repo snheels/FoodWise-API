ca0f0de7fbc3211f49bb83bc370a16a8484bb9fc
Shesa <shesadavinap03@gmail.com>
2026-04-04 15:53:18 +0700

    add route in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,11 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FoodController;
 
 Route::get('/user', [UserController::class, 'index']);
+
+Route::apiResource('foods', FoodController::class);
+// tambahan fitur
+Route::get('foods/dashboard', [FoodController::class, 'dashboard']);
+Route::get('foods/expiring-soon', [FoodController::class, 'expiringSoon']);
